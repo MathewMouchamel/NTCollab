5c702bf0cc54ff35c132585c2ef12646a317f134
MathewMouchamel <mathewmouchamel123@gmail.com>
2025-07-19 16:44:25 -0700

    add routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,12 @@ import { BrowserRouter } from "react-router-dom";
 import Index from "./pages/Index.jsx";
 import Documents from "./pages/Documents.jsx";
 import { AuthProvider } from "./AuthContext";
+import { Routes, Route } from "react-router-dom";
+
+<Routes>
+  <Route path="/" element={<Index />} />
+  <Route path="/documents" element={<Documents />} />
+</Routes>;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
