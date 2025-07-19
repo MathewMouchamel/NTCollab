ae57938bdd301cb5f55dbd32e98c03a67906879a
MathewMouchamel <mathewmouchamel123@gmail.com>
2025-07-19 15:32:13 -0700

    fix authContext

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import Index from "./pages/Index.jsx";
+import { AuthProvider } from "./AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Index />
+      <AuthProvider>
+        <Index />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
